88dac6b11d6223531026df0a66d8f57d5963594c
Vladimir <konceropyatovladimir@gmail.com>
2021-05-06 20:43:15 +0300

    add interpolation

--- 1.rb
+++ 1.rb
@@ -1,4 +1,3 @@
 puts "Your age?"
-age = gets
-age_month = age.to_i * 12
-puts " Your age is " + age_month.to_s
+age = gets.to_i
+puts "Your age is #{age * 12}"
---
+++ 2.rb
@@ -0,0 +1,4 @@
+puts "Your salary?"
+salary = gets.to_i
+tax_rate = 0.3
+puts "Tax: #{salary * tax_rate}"
